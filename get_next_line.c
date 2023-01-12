08d3e3edea4e9a19225058251db830d063ff717a
fgomez-d <fgomez-d@student.42malaga.com>
2023-01-12 11:13:30 +0100

    Rename count to line_len

--- get_next_line.c
+++ get_next_line.c
@@ -6,32 +6,32 @@
 /*   By: fgomez-d <fgomez-d@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/10 15:34:15 by fgomez-d          #+#    #+#             */
-/*   Updated: 2023/01/12 11:11:35 by fgomez-d         ###   ########.fr       */
+/*   Updated: 2023/01/12 11:13:04 by fgomez-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-int	count_until_endl(char *buf, char *line, int *count)
+int	count_until_endl(char *buf, char *line, int *line_len)
 {
-	while (*count - ft_strlen(line) < BUFFER_SIZE)
+	while (*line_len - ft_strlen(line) < BUFFER_SIZE)
 	{
-		if (buf[*count - ft_strlen(line)] == '\n')
+		if (buf[*line_len - ft_strlen(line)] == '\n')
 		{
-			*count += 1;
+			*line_len += 1;
 			return (1);
 		}
-		if (buf[*count - ft_strlen(line)] == '\0')
+		if (buf[*line_len - ft_strlen(line)] == '\0')
 		{
-			*count += 1;
+			*line_len += 1;
 			return (2);
 		}
-		*count += 1;
+		*line_len += 1;
 	}
 	return (0);
 }
 
-char	*recursive_get_line(char *buf, char *line, int fd, int *count)
+char	*recursive_get_line(char *buf, char *line, int fd, int *line_len)
 {
 	int			endln_found;
 	char		*new_line;
@@ -42,15 +42,15 @@ char	*recursive_get_line(char *buf, char *line, int fd, int *count)
 			return (free(line), NULL);
 	if (buf[0] == '\0')
 		return (line);
-	endln_found = count_until_endl(buf, line, count);
-	subbuf = ft_substr(buf, 0, *count - ft_strlen(line));
+	endln_found = count_until_endl(buf, line, line_len);
+	subbuf = ft_substr(buf, 0, *line_len - ft_strlen(line));
 	new_line = ft_strjoin(line, subbuf);
 	if (endln_found == 2)
 		return (ft_bzero(buf, BUFFER_SIZE), free(line), free(subbuf), (new_line));
 	if (BUFFER_SIZE <= ft_strlen(new_line) && !endln_found)
 	{
 		ft_bzero(buf, BUFFER_SIZE);
-		new_line = recursive_get_line(buf, new_line, fd, count);
+		new_line = recursive_get_line(buf, new_line, fd, line_len);
 		free(line);
 		free(subbuf);
 	}
@@ -61,18 +61,18 @@ char	*get_next_line(int fd)
 {
 	static char	buf[BUFFER_SIZE];
 	char		*line;
-	int			count;
+	int			line_len;
 
 	if (BUFFER_SIZE < 1)
 		return (NULL);
 	line = malloc(1);
 	ft_bzero(line, 1);
-	count = 0;
-	line = recursive_get_line(buf, line, fd, &count);
+	line_len = 0;
+	line = recursive_get_line(buf, line, fd, &line_len);
 	if (BUFFER_SIZE >= ft_strlen(line))
 	{
-		ft_memmove(buf, buf + count, BUFFER_SIZE - count);
-		buf[BUFFER_SIZE - count] = '\0';
+		ft_memmove(buf, buf + line_len, BUFFER_SIZE - line_len);
+		buf[BUFFER_SIZE - line_len] = '\0';
 	}
 	else
 		ft_bzero(buf, BUFFER_SIZE);
