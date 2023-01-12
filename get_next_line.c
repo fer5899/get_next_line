03fc18ff3f804239f907758fe2b22965e9720bf3
fgomez-d <fgomez-d@student.42malaga.com>
2023-01-12 11:12:17 +0100

    Clean up

--- get_next_line.c
+++ get_next_line.c
@@ -6,34 +6,11 @@
 /*   By: fgomez-d <fgomez-d@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/10 15:34:15 by fgomez-d          #+#    #+#             */
-/*   Updated: 2023/01/11 14:40:14 by fgomez-d         ###   ########.fr       */
+/*   Updated: 2023/01/12 11:11:35 by fgomez-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-
-// BORRAR
-#include <stdio.h>
-void	ft_putchar_fd(char c, int fd)
-{
-	write(fd, &c, 1);
-}
-void	ft_putnbr_fd(long n, int fd)
-{
-	if (n < 0)
-	{
-		ft_putchar_fd('-', fd);
-		n *= -1;
-	}
-	if (n < 10)
-		ft_putchar_fd(n + '0', fd);
-	else
-	{
-		ft_putnbr_fd(n / 10, fd);
-		ft_putnbr_fd(n % 10, fd);
-	}
-}
-// -------------------------
 
 int	count_until_endl(char *buf, char *line, int *count)
 {
@@ -65,33 +42,15 @@ char	*recursive_get_line(char *buf, char *line, int fd, int *count)
 			return (free(line), NULL);
 	if (buf[0] == '\0')
 		return (line);
-	// write(1, "\nbuffer: ", 10);
-	// write(1, buf, BUFFER_SIZE);
-
-	// Count until endl or end of buffer
 	endln_found = count_until_endl(buf, line, count);
-	
-	// write(1, "\ncount: ", 9);
-	// ft_putnbr_fd(*count, 1);
-	// write(1, "\n--- before ---\n", 17);
-	// write(1, line, ft_strlen(line));
-	
-	// Join buffer to line
 	subbuf = ft_substr(buf, 0, *count - ft_strlen(line));
 	new_line = ft_strjoin(line, subbuf);
-	
-	// write(1, "\n--- after ---\n", 16);
-	// write(1, new_line, ft_strlen(new_line));
-	// write(1, "\n ---\n\n", 7);
-	
 	if (endln_found == 2)
 		return (ft_bzero(buf, BUFFER_SIZE), free(line), free(subbuf), (new_line));
-
-	if (BUFFER_SIZE <= ft_strlen(new_line))
+	if (BUFFER_SIZE <= ft_strlen(new_line) && !endln_found)
 	{
 		ft_bzero(buf, BUFFER_SIZE);
-		if (!endln_found)
-			new_line = recursive_get_line(buf, new_line, fd, count);
+		new_line = recursive_get_line(buf, new_line, fd, count);
 		free(line);
 		free(subbuf);
 	}
